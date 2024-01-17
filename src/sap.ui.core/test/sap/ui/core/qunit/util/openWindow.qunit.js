66895747dcff6fc0b39a5033c2f85176286a541a
Restyled.io <commits@restyled.io>
2024-01-17 15:00:18 +0000

    Restyled by prettier

--- src/sap.ui.core/test/sap/ui/core/qunit/util/openWindow.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/util/openWindow.qunit.js
@@ -1,13 +1,13 @@
 /*global QUnit */
-sap.ui.define([ "sap/ui/util/openWindow" ], function(openWindow) {
-  "use strict";
-  QUnit.module("sap/ui/util/openWindow");
+sap.ui.define(["sap/ui/util/openWindow"], function (openWindow) {
+    "use strict";
+    QUnit.module("sap/ui/util/openWindow");
 
-  QUnit.test("Noopener noreferrer", function(assert) {
-    var oRes = openWindow("https://www.sap.com", "newWindow");
-    assert.ok(
-        oRes === null,
-        "Reference to the newly open window object is broken",
-    );
-  });
+    QUnit.test("Noopener noreferrer", function (assert) {
+        var oRes = openWindow("https://www.sap.com", "newWindow");
+        assert.ok(
+            oRes === null,
+            "Reference to the newly open window object is broken",
+        );
+    });
 });
